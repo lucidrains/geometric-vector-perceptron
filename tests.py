9b02000a2eede45a8c66e1cd999abacb2dde4f99
Phil Wang <lucidrains@gmail.com>
2021-01-13 13:23:26 -0800

    equivariance test

--- tests.py
+++ tests.py
@@ -1,3 +1,27 @@
+import torch
+from geometric_vector_perceptron import GVP
+
+TOL = 1e-2
+
+def random_rotation():
+    q, r = torch.qr(torch.randn(3, 3))
+    return q
 
 def test_equivariance():
-    assert True
+    R = random_rotation()
+
+    model = GVP(
+        dim_v = 1024,
+        dim_n = 512,
+        dim_m = 512,
+        dim_u = 1024
+    )
+
+    feats = torch.randn(1, 512)
+    coors = torch.randn(1, 1024, 3)
+
+    feats_out, coors_out = model(feats, coors)
+    feats_out_r, coors_out_r = model(feats, coors @ R)
+
+    err = ((coors_out @ R) - coors_out_r).max()
+    assert err < TOL, 'equivariance must be respected'
