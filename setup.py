e10dd1849854c545e51acebb1b86adb513771102
Phil Wang <lucidrains@gmail.com>
2021-02-21 09:16:08 -0800

    0.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'geometric-vector-perceptron',
   packages = find_packages(),
-  version = '0.0.6',
+  version = '0.0.7',
   license='MIT',
   description = 'Geometric Vector Perceptron - Pytorch',
   author = 'Phil Wang and Eric Alcaide',
