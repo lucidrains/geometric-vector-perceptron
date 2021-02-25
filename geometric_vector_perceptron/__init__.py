ffa1102053134e8ad5130a16be2a7108ffd7deea
hypnopump <ericalcaide1@gmail.com>
2021-02-25 11:52:21 +0100

    adding viisbilityfrom init

--- geometric_vector_perceptron/__init__.py
+++ geometric_vector_perceptron/__init__.py
@@ -1,1 +1,1 @@
-from geometric_vector_perceptron.geometric_vector_perceptron import GVP, GVPDropout, GVPLayerNorm, GVP_MPNN
+from geometric_vector_perceptron.geometric_vector_perceptron import GVP, GVPDropout, GVPLayerNorm, GVP_MPNN, GVP_Network
